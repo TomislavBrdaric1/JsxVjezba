0e9bbc44ed6619aba0e871498744e12baabf7999
Tomislav <tom@tom.com>
2022-12-21 11:25:02 +0100

    vjezba

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 const App = () => {
-    return <div></div>;
+    const number = 6;
+
+    return (
+        <div>
+            {number % 2 !== 0 && <h1>The number is not even</h1>}
+            {!(number % 2) && <h1>The number is even</h1>}
+        </div>
+    );
 };
 
 export default App;
